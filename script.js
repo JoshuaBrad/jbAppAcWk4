0ad2ea0fac36f044bed952907235a38b329c3f6a
JoshuaBrad <85682477+JoshuaBrad@users.noreply.github.com>
2025-06-09 22:04:14 +0200

    .

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ function add(){
     var num1;
     var num2;
 
-    num1 = prompt("Enter first number to add: ",0);
-    num2 = prompt("Enter second number to add: ",0);
+    num1 = parseInt(prompt("Enter first number to add: ",0));
+    num2 = parseInt(prompt("Enter second number to add: ",0));
 
     alert(num1 + " + " + num2 + " = " + (num1+num2));
     console.log(num1 + " + " + num2 + " = " + (num1+num2))
